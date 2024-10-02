8cb988ef6f894f93f102e727acdf814ec3610078
Merve Sayın <mervetek.bilgmuh@gmail.com>
2024-10-02 15:28:25 +0300

    first commit

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reactive;
 using AppointmentSystem.Utils;
 using AppointmentSystem.Views;
@@ -16,6 +17,7 @@ public class MainWindowViewModel : ViewModelBase
     {
         var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         Appointments = FileOperations.GetAllAppointments(Path.Combine(documentsPath + "/appointment.json"));
+        BuildAppointmentList(Appointments);
         
         CreateAppointment = ReactiveCommand.Create(() =>
         {
@@ -29,7 +31,26 @@ public class MainWindowViewModel : ViewModelBase
 
     [Reactive] public AvaloniaList<AppointmentRegisterViewModel>? Appointments { get; set; }
     [Reactive] private AppointmentRegisterViewModel NewAppointment { get; set; } = new();
-    
+
+    [Reactive] public AvaloniaList<AppointmentItemViewModel>? AppointmentList { get; set; }
+
     public ReactiveCommand<Unit, Unit> CreateAppointment { get; }
     public ReactiveCommand<Unit, Unit> ListAppointments { get; }
+
+    private void BuildAppointmentList(AvaloniaList<AppointmentRegisterViewModel>? appointments)
+    {
+        if (Appointments == null)
+            return;
+        
+        foreach (var item in Appointments.Select(appointment => new AppointmentItemViewModel
+                 {
+                     CustomerName = appointment.CustomerName,
+                     CustomerLastName = appointment.CustomerLastName,
+                     AppointmentDate = appointment.AppointmentDate,
+                     IsFatura = appointment.IsFatura,
+                     AppointmentType = appointment.AppointmentType
+                 }))
+        {
+        }
+    }
 }
